26d37e84efcdd9ff830d24796de7a6750b70cde4
Vince <starryblue7@gmail.com>
2021-11-22 23:33:04 -0800

    format footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+const styles = {
+    textAlign: "center",
+    fontSize: "60%",
+    position: "absolute",
+    bottom: 5,
+    transform: "translateX(-50%)",
+    left: "50%"
+}
+
 function Footer() {
     return (
-        <footer>&copy; Vince Lee 2021</footer>
+        <footer style={styles}>&copy; Vince Lee 2021</footer>
     )
 }
 
