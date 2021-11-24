d361aa6b13e3d3680f05a17fe623a94154ba9190
Vince <starryblue7@gmail.com>
2021-11-23 17:05:21 -0800

    add project images, map project data to Project components

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,39 +1,19 @@
 import React from 'react';
+import Project from '../Project';
+import Fade from 'react-reveal';
+
+import projectData from '../../data/projectData.json';
 
 function Portfolio() {
-    const styles = {
-        h1: {
-            fontSize: "300%",
-            color: "white"
-        },
-        placeholder: {
-            height: "100vh",
-            width: "100vw",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "LightSlateGrey"
-        },
-        image: {
-            height: 200,
-        }
-    }
-
     return (
-        <div className="placeholder" style={styles.placeholder}>
-            <h1 style={styles.h1}>Coming Soon!</h1>
-            <div>
-                <img style={styles.image} src="https://www.pngplay.com/wp-content/uploads/2/Squirrel-PNG-Photos.png" alt="Squirrel" />
-                <img 
-                    className="App-logo" 
-                    style={styles.image}
-                    src="https://www.pngplay.com/wp-content/uploads/4/Acorn-PNG-Images-HD.png" 
-                    alt="Acorn"
-                />
-            </div>
-            
-        </div>
+        <section>
+            <h2>Portfolio</h2>
+            <Fade cascade >
+                {projectData.map(project => (
+                    <Project project={project} />
+                ))}
+            </Fade>
+        </section>
     );
 }
 
