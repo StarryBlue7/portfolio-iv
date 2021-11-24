9c489a9dbd53e5cfe18b9863b7137fd8861e8bcb
Vince <starryblue7@gmail.com>
2021-11-23 21:01:45 -0800

    add portfolio/projects page styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
 
-function Project() {
+const styles = {
+    h4: {
+        marginBottom: 0,
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end'
+    },
+    card: {
+        flex: '1 1 60%',
+    },
+    image: {
+        objectFit: 'cover',
+        width: '100%'
+    },
+}
+
+function Project({project}) {
     return (
-        <div>Project</div>
+        <summary style={styles.card}>
+            <h4 style={styles.h4}>
+                <a target="_blank" rel="noreferrer" href={project.deployed_url}>{project.title}</a>
+                <a target="_blank" rel="noreferrer" href={project.github_url}><FaGithub /></a>
+            </h4>
+            <a target="_blank" rel="noreferrer" href={project.deployed_url} style={styles.imgBox}>
+                <img alt={project.title} src={project.img_url} style={styles.image} />
+            </a>
+        </summary>
     )
 }
 
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Project from '../Project';
-import Fade from 'react-reveal';
 
 import projectData from '../../data/projectData.json';
 
+const styles = {
+    projects: {
+        display: 'flex',
+        flexFlow: 'row wrap-reverse',
+    },
+    project: {
+        flex: '1 1 30%'
+    }
+}
+
 function Portfolio() {
     return (
-        <section>
+        <section className="page">
             <h2>Portfolio</h2>
-            <Fade cascade >
-                {projectData.map(project => (
-                    <Project project={project} />
+            <article style={styles.projects}>
+                {projectData.map((project, i) => (
+                    <Project project={project} key={i} style={styles.project} />
                 ))}
-            </Fade>
+            </article>
         </section>
     );
 }
