b2dbb56c3cc8d7e061075b4e6721e6d8b4c8657d
Vince <starryblue7@gmail.com>
2021-11-23 19:55:03 -0800

    add resume pdf embed styling

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import resume from '../../assets/CV_VinceLee_2021.pdf'
 
+const styles = {
+    resume: {
+        width: "100%",
+        height: "60vh",
+    }
+}
+
 function Resume() {
     return (
         <section className="page">
-            <h2>Résumé</h2>
-            <embed src={resume} />
+            <h2>Résumé & CV</h2>
+            <embed src={resume} style={styles.resume} />
         </section>
     )
 }
