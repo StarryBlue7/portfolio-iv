4d5301f7155c4ff6051e73cd8bbb3702a7db0245
Vince <starryblue7@gmail.com>
2021-11-29 13:59:19 -0800

    prevent x-overflow

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import Main from './components/Main';
 import Particles from 'react-tsparticles';
 import config from './data/particles.json';
 
+const styles = {
+  background: {
+    overflowX: 'hidden'
+  }
+}
+
 function App() {
   return (
-    <div id="body">
+    <div id="body" style={styles.background}>
       <Particles
         id="tsparticles"
         options={config}
