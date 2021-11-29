a0946f7c92d281e0724cd47f97d750aa1f8e0ea8
Vince <starryblue7@gmail.com>
2021-11-29 14:00:21 -0800

    add skills to Resume component

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,12 +8,25 @@ const styles = {
     }
 }
 
-// Resume page
+// Resume & skills page
 function Resume() {
     return (
         <section className="page">
             <h2>Résumé & CV</h2>
             <embed src={resume} style={styles.resume} />
+
+            <h2>Skills</h2>
+            <ul>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Java</li>
+                <li>SQL</li>
+                <li>MongoDB</li>
+                <li>jQuery</li>
+                <li>CSS/HTML5</li>
+                <li>Responsive Web Design</li>
+            </ul>
         </section>
     )
 }
