a2a9afba642505d39925df33e66b7e440799452b
Vince Lee <vince.lee.dev@gmail.com>
2024-09-18 10:37:54 -0700

    update year

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import Main from './components/Main';
-import Particles from 'react-tsparticles';
-import config from './data/particles.json';
+import Main from "./components/Main";
+import Particles from "react-tsparticles";
+import config from "./data/particles.json";
 
 const styles = {
-  background: {
-    overflowX: 'hidden'
-  }
-}
+    background: {
+        overflowX: "hidden",
+    },
+};
 
 function App() {
-  return (
-    <div id="body" style={styles.background}>
-      <Particles
-        id="tsparticles"
-        options={config}
-      />
-      <Main />
-    </div>
-    
-  );
+    return (
+        <div id="body" style={styles.background}>
+            <Particles id="tsparticles" options={config} />
+            <Main />
+        </div>
+    );
 }
 
 export default App;
